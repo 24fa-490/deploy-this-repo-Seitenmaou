1b079ad5bece4089b1a049ba37f4d45d7e854f2f
Euri Tanaka <dev@mugendiary.com>
2024-10-25 18:17:14 -0700

    local connection test

--- src/routes/fromDB/+page.svelte
+++ src/routes/fromDB/+page.svelte
@@ -1,21 +1,20 @@
 <script>
 	export let data;
 	//console.log({data});
-
 </script>
 
 <h1>Fetched from Database</h1>
 
 {#each data.containers as container}
 	<div>
-		<span class="id">{container.containernumber}</span>
-		<span class="name">{container.nameofship}</span>
-		<span class="size">{container.containersize}</span>
-		<span class="date">{container.datecontainershipped}</span>
+		<div class="id">{container.containernumber}</div>
+		<div class="shipname">{container.nameofship}</div>
+		<div class="date">{container.datecontainershipped}</div>
+		<div class="size">{container.containersize}</div>
 	</div>
 {/each}
 
-<span class=i>(database lives in PostgreSQL)</span>
+<span class="i">(database lives in PostgreSQL)</span>
 
 <style>
 	.id {
